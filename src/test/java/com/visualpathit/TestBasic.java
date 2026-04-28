f05e8e8a8ae1369269d5d2a82e3c74435d46dacf
tcollins520 <tcollins520@gmail.com>
2026-04-27 23:58:13 -0400

    Add basic unit test

---
+++ src/test/java/com/visualpathit/TestBasic.java
@@ -0,0 +1,12 @@
+package com.visualpathit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestBasic {
+
+    @Test
+    void testSomething() {
+        assertTrue(true);
+    }
+}
